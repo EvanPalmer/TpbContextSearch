f8dab1d2c873c99cc40b214016d93bc26134bffa
Evan Palmer <evan.palmer@gmail.com>
2013-03-12 23:38:01 +1100

    Bug fixes
    Added a "loading now" child context menu element
    Added a "no results found" element
    Display size of download in context menu items.

--- doTheWork.js
+++ doTheWork.js
@@ -1,11 +1,10 @@
 // TODO:
 // Could use Url Filter to do something to IMDB http://developer.chrome.com/extensions/events.html#type-UrlFilter (parse when the dom is loaded)
-// Need a "loading now" child context menu element
-// Need a "no results found" element
-// Display size of download in context menu items.
 // Clean up folder structure, and remove unused files
 // Sometimes it stops working - the search term doesn't update and the onclick on the" Search TBP for XXX" does nothing
-// 
+// What happens when you don't have uTorrent installed?
+// How to add preferences?? Like, prefer HQ, prefer most recent, exclude XXX, 
+// Give the files better names.
 
 function genericOnClick(searchText) {
 	var searchUrl = 'http://thepiratebay.se/search/' + encodeURIComponent(searchText) + '/0/7/0'
@@ -23,11 +22,11 @@ function loadMagnetLink(magnetLink){
 
 var parentId = 0;
 var lastSearchTerm = '';
-
+var loadingId = 0;
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         switch (request.directive) {
-        case "page-clicked":
+        case 'page-clicked':
             // execute the content script
             console.log(request.selectedText);
            	if((request.selectedText.length <= 0)||(lastSearchTerm === request.selectedText)){
@@ -35,26 +34,40 @@ chrome.extension.onMessage.addListener(
            		return;
            	}
            	lastSearchTerm = request.selectedText;
-			var title = "Search TPB for '" + request.selectedText + "'";
-			chrome.contextMenus.removeAll(function(){});
-			parentId = chrome.contextMenus.create({"title": title, "contexts":["selection"], "onclick": function() { genericOnClick(request.selectedText); }});
+      			var title = "Search TPB for '" + request.selectedText + "'";
+      			chrome.contextMenus.removeAll(function(){});
+      			parentId = chrome.contextMenus.create({"title": title, "contexts":["selection"], "onclick": function() { genericOnClick(request.selectedText); }});
+      			loadingId = chrome.contextMenus.create({'title': 'Loading... try again.', 'parentId': parentId, 'contexts':['selection'], 'onclick': function() { alert("TPB haven't told me what they have yet. Try giving it another click.... NOW!"); }});
 
-			var searchUrl = 'http://thepiratebay.se/search/' + encodeURIComponent(request.selectedText) + '/0/7/0'
+      			var searchUrl = 'http://thepiratebay.se/search/' + encodeURIComponent(request.selectedText) + '/0/7/0'
 
-			var xhr = new XMLHttpRequest();
-			xhr.open("GET", searchUrl, true);
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState == 4) {
-					// remember to handle no results
-					$(xhr.responseText).find('a[title="Download this torrent using magnet"]:lt(5)').closest('td').each(function(i, e) { 
-						//alert(e);
-						console.log($(e).find('a.detLink').text());
-						console.log($(e).find('a[title="Download this torrent using magnet"]').attr('href'));
-						chrome.contextMenus.create({'title': $(e).find('a.detLink').text(), 'parentId': parentId, 'contexts':['selection'], 'onclick': function() { loadMagnetLink($(e).find('a[title="Download this torrent using magnet"]').attr('href')); }});
-					});
-				}
-			}
-			xhr.send();
+      			var xhr = new XMLHttpRequest();
+      			xhr.open('GET', searchUrl, false);
+      			xhr.onreadystatechange = function() {
+      				if (xhr.readyState == 4) {
+                // Get rid of the menu item claiming we're loading (because we have a response now)
+      					chrome.contextMenus.remove(loadingId);
+                var resultTds = $(xhr.responseText).find('a[title="Download this torrent using magnet"]:lt(5)').closest('td');
+
+                // Handle no results from TPB
+                if(resultTds.length === 0)  {
+                  chrome.contextMenus.create({'title': "TPB a'int got nothing.", 'parentId': parentId, 'contexts':['selection'], 'onclick': $.noop});
+                  return;
+                }
+
+                // Sweet! We have some results!
+      					resultTds.each(function(i, e) { 
+      						var description = $(e).find('font.detDesc').text();
+      						var sizeExtractionRegex = /.+Size (.+?),.+/i;
+      						var size = sizeExtractionRegex.exec(description)[1];
+      						var linkName = $(e).find('a.detLink').text();
+      						var contextMenuTitle = '(' + size + ') ' + linkName;
+      						var magnetUrl = $(e).find('a[title="Download this torrent using magnet"]').attr('href');
+      						chrome.contextMenus.create({'title': contextMenuTitle, 'parentId': parentId, 'contexts':['selection'], 'onclick': function() { loadMagnetLink(magnetUrl); }});
+      					});
+      				}
+      			}
+      			xhr.send();
 
             sendResponse({}); // sending back empty response to sender
             break;
@@ -67,7 +80,7 @@ chrome.extension.onMessage.addListener(
 
 chrome.tabs.onUpdated.addListener(function(tab) {
 	chrome.tabs.executeScript(tab.id, { // defaults to the current tab
-		file: "contentscript.js", // script to inject into page and run in sandbox
+		file: 'contentscript.js', // script to inject into page and run in sandbox
 		allFrames: false // When true, this injects script into iframes in the page and doesn't work before 4.0.266.0. We actually don't want this here.
 	});
 
@@ -78,3 +91,4 @@ chrome.tabs.onUpdated.addListener(function(tab) {
 chrome.tabs.onCreated.addListener(function(tab) {
 	console.log('tab created');
 });
+
