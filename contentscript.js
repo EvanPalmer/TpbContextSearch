ce9b93a519e64e4029de5afd9af83a6cff8321ad
Evan Palmer <evan.palmer@gmail.com>
2013-04-17 19:47:59 +1000

    Added download box for IMDB
    Added download box for IMDB - before refactor

--- contentscript.js
+++ contentscript.js
@@ -4,7 +4,7 @@
 		if(thisIsImdb){
 			// add bootstrap JS from CDN
 			$.getScript("//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js", function(){
-			    $.noop;//alert("Running boostrap!");
+			    $.noop;
 			});
 
 			// make room for the new DDL
@@ -12,24 +12,37 @@
 
 			var searchTerm = $('h1.header').text().replace(/\s+/g, ' ').trim();
 
-			$('#prometer_container').append('<div id="tpb-context-search-download-container"><ul class="nav nav-pills">\
-											  <li class="dropdown">\
-											    <a class="dropdown-toggle"\
-											       data-toggle="dropdown"\
-											       href="#">\
-											        Download\
-											        <b class="caret"></b>\
-											      </a>\
-											    <ul class="dropdown-menu">\
-											      <li><a target="_blank" href="http://thepiratebay.se/search/' + encodeURIComponent(searchTerm) + '/0/7/0">Search TPB for "' + searchTerm + '"</a></li>\
-												  <li class="divider"><a href="#">(1.46Mb) The Shawshank Redemption QWE0qwe</a></li>\
-												  <li><a href="#">(1.46Mb) The Shawshank Redemption QWE0qwe</a></li>\
-												  <li><a href="#">(7.26Gb) The.Shawshank.Redemption BluRay</a></li>\
-												  <li><a href="#">(700Mb) The Shawshank Redemption Crap quality</a></li>\
-												  <li><a href="#">(1.46Mb) The Shawshank Redemption QWE0qwe</a></li>\
-											    </ul>\
-											  </li>\
-											</ul>');
+			var searchUrl = 'http://thepiratebay.se/search/' + encodeURIComponent(searchTerm) + '/0/7/0'
+			$.get(searchUrl, function(data){
+				debugger;
+
+                var resultTds = $(data).find('a[title="Download this torrent using magnet"]:lt(5)').closest('td');
+
+                // Handle no results from TPB
+                if(resultTds.length === 0)  {
+                  return;
+                }
+
+                // Sweet! We have some results!
+
+				$('#prometer_container').append($('<ul class="nav nav-pills">')
+					.append($('<li class="dropdown">')
+						.append($('<a class="dropdown-toggle" data-toggle="dropdown" href="#">Download<b class="caret"></b>'))
+						.append($('<ul class="dropdown-menu">')
+							.append($('<li>').append('<a target="_blank" href="http://thepiratebay.se/search/' + encodeURIComponent(searchTerm) + '/0/7/0">Search TPB for "' + searchTerm + '"</a>'))
+							.append($('<li class="divider">')))));
+
+				resultTds.each(function(i, e) { 
+					var description = $(e).find('font.detDesc').text();
+					var sizeExtractionRegex = /.+Size (.+?),.+/i;
+					var size = sizeExtractionRegex.exec(description)[1];
+					var linkName = $(e).find('a.detLink').text();
+					var contextMenuTitle = '(' + size + ') ' + linkName;
+					var magnetUrl = $(e).find('a[title="Download this torrent using magnet"]').attr('href');
+					
+					$('#prometer_container .dropdown-menu').append($('<li>').append($('<a>').attr('href', magnetUrl).append(contextMenuTitle)));
+				});
+			});
 		};
 
 		window.onmouseup = function(e){ 
